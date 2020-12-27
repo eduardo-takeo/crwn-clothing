339b671fe3eb4eeec7d0fd839681c4cdaf3c06f9
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2020-12-27 15:21:44 -0300

    Add "Add to cart" button on card-item component
    Added "Add to cart" button inside card component on Shop page;
    Implemented hover effect to give a fadeOut effect on cards (due to css
    inheritance button also is affected - need to refactor later)

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './collection-item.styles.scss';
+import CustomButton from 'components/custom-button/custom-button.component';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  const [showAddToCart, setShowAddToCart] = useState(false);
+
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        onMouseEnter={() => setShowAddToCart(true)}
+        onMouseLeave={() => setShowAddToCart(false)}
+      >
+        {showAddToCart &&
+          <CustomButton inverted className='custom-button'> ADD TO CART</CustomButton>
+        }
+      </div>
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
     </div>
-  </div>
-);
+  )
+}
 
 export default CollectionItem;
