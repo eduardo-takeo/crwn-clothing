66a708586ba31dc1eeb9f9708ab78210c9197b98
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-02-13 19:04:22 -0300

    Display items amount on cart icon
    The cart icon now should display the number of items that were added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import './cart-icon.styles.scss';
-import { ReactComponent as ShoppingIcon } from 'assets/shopping-bag.svg';
-import { toggleModal } from 'redux/ducks/cart';
+import "./cart-icon.styles.scss";
+import { ReactComponent as ShoppingIcon } from "assets/shopping-bag.svg";
+import { toggleModal } from "redux/ducks/cart";
 
 function CartIcon() {
-    const dispatch = useDispatch();
-    const { isVisible } = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const { isVisible, items } = useSelector((state) => state.cart);
 
-    return (
-        <div className='cart-icon' onClick={() => dispatch(toggleModal(!isVisible))}>
-            <ShoppingIcon className='shopping-icon' />
-            <span className="item-count">0</span>
-        </div>
-    )
+  return (
+    <div
+      className="cart-icon"
+      onClick={() => dispatch(toggleModal(!isVisible))}
+    >
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{items.length}</span>
+    </div>
+  );
 }
 
-export default CartIcon
+export default CartIcon;
