8c4d62945d6711b208c635cb7cc39cdf16addb99
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-02-27 14:31:52 -0300

    Add redux-persist lib

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
 
-import Routes from './routes';
-import store from './redux/store';
+import Routes from "./routes";
+import { store, persistor } from "./redux/store";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
     </Provider>
-
-  )
+  );
 }
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,30 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
-import userReducer from './ducks/user';
-import cartReducer from './ducks/cart';
+import userReducer from "./ducks/user";
+import cartReducer from "./ducks/cart";
 
 const middlewares = [logger];
 
-const rootReducer = combineReducers({
-    user: userReducer,
-    cart: cartReducer
-})
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(...middlewares)
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  applyMiddleware(...middlewares)
 );
+
+export const persistor = persistStore(store);
 
 export default store;
