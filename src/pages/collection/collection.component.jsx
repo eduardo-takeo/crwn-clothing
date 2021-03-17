50772fb3ece5d21c17a17bee02ed48925eaeb29f
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-03-16 22:03:16 -0300

    Create collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,17 +1,24 @@
+import CollectionItem from "components/collection-item/collection-item.component";
 import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { currentCollectionSelector } from "../../redux/ducks/shop";
+import "./collection.styles.scss";
 
 export default function CollectionPage() {
   const { collectionName } = useParams();
-  const collection = useSelector(currentCollectionSelector(collectionName));
-
-  // TODO: Build component based on collection
+  const { items, title } = useSelector(
+    currentCollectionSelector(collectionName)
+  );
 
   return (
-    <div>
-      <h1>Collection Page</h1>
+    <div className="collection-page">
+      <h1 className="title">{title}</h1>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
